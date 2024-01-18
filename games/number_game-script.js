b97cca2ab3b8cc24608cd4c725d63661eb024df1
polen <paulineflores.exe@gmail.com>
2024-01-18 23:37:45 +0800

    gumagana nagit add .!

--- games/number_game-script.js
+++ games/number_game-script.js
@@ -1,4 +1,3 @@
-// Get the DOM elements and initialize the game
 const input = document.querySelector("input"),
   guess = document.querySelector(".guess"),
   checkButton = document.querySelector("button"),
@@ -11,7 +10,7 @@ let randomNum = Math.floor(Math.random() * 100);
 chance = 10;
 
 // Listen for the click event on the check button
-checkButton.addEventListener("click", () => {
+document.getElementById('checkbtn').addEventListener("click", () => {
   // Decrement the chance variable on every click
   chance--;
   // Get the value from the input field
@@ -19,18 +18,18 @@ checkButton.addEventListener("click", () => {
   // Check if the input value is equal to the random number
   if (inputValue == randomNum) {
     // Update guessed number, disable input, check button text and color.
-    [guess.textContent, input.disabled] = ["Congratulations", true];
-    [checkButton.textContent, guess.style.color] = ["Replay", "#333"];
+    [guess.textContent, input.disabled, guess.style.color] = ["Congratulations", true, "#B8C6DC"];
+    [checkButton.textContent, guess.style.color] = ["Replay", "B8C6DC"];
     //Check if input value is > random number and within 1-99 range.
   } else if (inputValue > randomNum && inputValue < 100) {
     // Update the guess text and remaining chances
     [guess.textContent, remainChances.textContent] = ["Your guess is high", chance];
-    guess.style.color = "#333";
+    guess.style.color = "#B8C6DC";
     //Check if input value is < random number and within 1-99 range.
   } else if (inputValue < randomNum && inputValue > 0) {
     // Update the guessed number text and remaining chances
     [guess.textContent, remainChances.textContent] = ["Your guess is low", chance];
-    guess.style.color = "#333";
+    guess.style.color = "#B8C6DC";
     // If the input value is not within the range of 1 to 99
   } else {
     // Update the guessed number text, color and remaining chances
